4cac173a8598007e37f71c8ea968e89acbf55cd4
Mathys Sclafer <mathys.sclafer@edu.esiee-it.fr>
2024-10-07 16:58:50 +0200

    TP 1 Boucle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int Numbers[] = {3, 9, 13, 64, 37, 83, 60};
+        float Moyenne = 0;
+
+        for(int i = 0; i < Numbers.length; i++){
+            System.out.println(Numbers[i]);
+            Moyenne += Numbers[i];
+        }
+
+        System.out.println("Moyenne: " + Moyenne/Numbers.length);
     }
 }
