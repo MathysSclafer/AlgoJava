e15b7a3c1b0d8e0a14ac99103b2366969376e8d0
Mathys Sclafer <mathys.sclafer@edu.esiee-it.fr>
2024-10-08 10:28:17 +0200

    Ajout TP3 (commentaire pour javaDoc)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+/**
+ * Affiche la moyenne et appelle des fonctions de la class TP2
+ */
 public class Main {
+    /**
+     * Affiche la moyenne et appelle des fonctions de la class TP2
+     */
     public static void main(String[] args) {
         int[] Numbers = {11, 27, 13, 64, 37, 83, 60};
-        float Moyenne = 0;
+        float AllNumbers = 0;
 
         TP2 tp2 = new TP2();
 
         for(int i = 0; i < Numbers.length; i++){
-            Moyenne += Numbers[i];
+            AllNumbers += Numbers[i];
         }
 
-        System.out.println("Moyenne: " + Moyenne/Numbers.length);
+        System.out.println("Moyenne: " + AllNumbers/Numbers.length);
         tp2.PrintTable(Numbers);
         System.out.println("Plus gros chiffre: " + tp2.ReturnHighestNumber(Numbers, Numbers[0]));
         System.out.println(tp2.isNumberLessThan10(Numbers));
--- src/TP2.java
+++ src/TP2.java
@@ -1,16 +1,31 @@
 import java.util.Collections;
 
+/**
+ * Regroupe des fonctions
+ */
 public class TP2 {
 
+    /**
+     * Affiche un tableau dans la console
+     * @param Tableau le tableau qui va être affiché
+     */
     public static void PrintTable(int[] Tableau){
         for(int i = 0; i < Tableau.length; i++){
             System.out.println(Tableau[i]);
         }
     }
 
+    /**
+     * Retourne la valeur max d'un tableau
+     * @param Tableau Tableau utilisé pour avoir la valeur max
+     * @param TableauIndex0 Chiffre max par defaut
+     * @return la valeur max du tableau
+     */
     public static int ReturnHighestNumber(int[] Tableau, int TableauIndex0){
+        // Définie que max est égal a l'index 0 du tableau
         int max = TableauIndex0;
         for(int i = 0; i < Tableau.length; i++){
+            // Si le chiffre du tableau > max actuelle alors on remplace l'ancien max par le nouveau
             if(Tableau[i] > max){
                 max = Tableau[i];
             }
@@ -18,6 +33,11 @@ public class TP2 {
         return max;
     }
 
+    /**
+     * Retourne Vrai si au moins 1 chiffre est inferieu a 10
+     * @param Tableau utilisé pour récuperer les chiffres
+     * @return si un chiffre est inferieure a 10 ou non
+     */
     public static boolean isNumberLessThan10(int[] Tableau){
         for(int i = 0; i < Tableau.length; i++){
             if(Tableau[i] < 10){
